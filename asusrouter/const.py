612129b233b5780f3d5e5401ee243a7201c0ea57
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2022-04-30 18:17:38 +0200

    Move constants from helpers.py

--- asusrouter/const.py
+++ asusrouter/const.py
@@ -130,3 +130,69 @@ PORT_TYPE = [
 KEY_NVRAM_GET = "nvram_get"
 
 
+### DEVICEMAP
+
+# These values are just stored directly in this order in the corresponding node
+DEVICEMAP_BY_INDEX = {
+    "WAN": { "wan": ["status", "sbstatus", "auxstatus", ], },
+    "WAN0": { "first_wan": ["status", "sbstatus", "auxstatus", ], },
+    "WAN1": { "second_wan": [ "status", "sbstatus", "auxstatus", ], },
+    "USB": { "usb": [ "status", ], },
+}
+
+
+#Not implemented yet
+#psta:wlc_state=0;wlc_state_auth=0;
+DEVICEMAP_SPECIAL = {
+    "WAN": {
+        "psta": [
+            "wlc_state",
+            "wlc_state_auth",
+        ],
+    },
+}
+
+# These values are stored as "key=value"
+DEVICEMAP_GENERAL = {
+    "WAN": {
+        "wan": [ "monoClient", "wlc_state", "wlc_sbstate", "wifi_hw_switch", "ddnsRet", "ddnsUpdate", "wan_line_state", "wlan0_radio_flag", "wlan1_radio_flag", "wlan2_radio_flag", "data_rate_info_2g", "data_rate_info_5g", "data_rate_info_5g_2", "wan_diag_state", "active_wan_unit", "wlc0_state", "wlc1_state", "rssi_2g", "rssi_5g", "rssi_5g_2", "link_internet", "wlc2_state", "le_restart_httpd", ],
+    },
+    "VPN": {
+        "vpn": [ "vpnc_proto", "vpnc_state_t", "vpnc_sbstate_t", "vpn_client1_state", "vpn_client2_state", "vpn_client3_state", "vpn_client4_state", "vpn_client5_state", "vpnd_state", "vpn_client1_errno", "vpn_client2_errno", "vpn_client3_errno", "vpn_client4_errno", "vpn_client5_errno", ],
+    },
+    "SYS": {
+        "sys": [ "uptimeStr", ],
+    },
+    "QTN": {
+        "qtn": [ "qtn_state", ],
+    },
+    "USB": {
+        "usb": [ "modem_enable", ],
+    },
+    "WAN0": {
+        "wan": [ "wan0_enable", "wan0_realip_state", "wan0_ipaddr", "wan0_realip_ip", ],
+    },
+    "WAN1": {
+        "wan": [ "wan1_enable", "wan1_realip_state", "wan1_ipaddr", "wan1_realip_ip", ],
+    },
+    "SIM": {
+        "sim": [ "sim_state", "sim_signal", "sim_operation", "sim_isp", "roaming", "roaming_imsi", "sim_imsi", "g3err_pin", "pin_remaining_count", "modem_act_provider", "rx_bytes", "tx_bytes", "modem_sim_order", ],
+    },
+    "DHCP": {
+        "dhcp": [ "dnsqmode", ],
+    },
+    "DIAG": {
+        "diag": [ "diag_dblog_enable", "diag_dblog_remaining", ]
+    }
+}
+
+# This should be cleared from the data
+DEVICEMAP_CLEAR = {
+    "WAN": {
+        "data_rate_info_2g": "\"",
+        "data_rate_info_5g": "\"",
+        "data_rate_info_5g_2": "\"",
+    }
+}
+
+
