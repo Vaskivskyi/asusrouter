ff1f7421fd3a4d9aa05180e313ce44c838504e92
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2022-10-28 17:21:04 +0200

    Retry on page load before throwing an exception

--- asusrouter/connection.py
+++ asusrouter/connection.py
@@ -161,6 +161,8 @@ class Connection:
                 if "404 Not Found" in string_body:
                     raise AsusRouter404()
         except aiohttp.ClientError as ex:
+            if not retry:
+                return self.async_load(endpoint, retry=True)
             raise AsusRouterConnectionError(ex)
         except Exception as ex:
             raise ex
