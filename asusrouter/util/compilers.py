0a600b8ab9cf622e65c4761e4b03054111865c4c
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2023-02-16 22:18:05 +0100

    Fix empty and None values in port forwarding rules (#281)

--- asusrouter/util/compilers.py
+++ asusrouter/util/compilers.py
@@ -164,7 +164,27 @@ def port_forwarding(data: list[PortForwarding]) -> dict[str, str]:
 
     result = ""
     for rule in data:
-        result += f"<{rule.name}>{rule.port_external}>{rule.ip}>{rule.port}>{rule.protocol}>{rule.ip_external}"
+        name = str()
+        if rule.name is not None and rule.name != "None":
+            name = rule.name
+        port_external = str()
+        if rule.port_external is not None and rule.port_external != "None":
+            port_external = rule.port_external
+        ip = str()
+        if rule.ip is not None and rule.ip != "None":
+            ip = rule.ip
+        port = str()
+        if rule.port is not None and rule.port != "None":
+            port = rule.port
+        protocol = str()
+        if rule.protocol is not None and rule.protocol != "None":
+            protocol = rule.protocol
+        ip_external = str()
+        if rule.ip_external is not None and rule.ip_external != "None":
+            ip_external = rule.ip_external
+
+
+        result += f"<{name}>{port_external}>{ip}>{port}>{protocol}>{ip_external}>"
 
     return {
         KEY_PORT_FORWARDING_LIST: result,
