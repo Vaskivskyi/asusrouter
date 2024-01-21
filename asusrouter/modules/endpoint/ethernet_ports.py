2217d9663b6bcfa0a2ea1900f6d933fa31722c51
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2024-01-21 10:17:26 +0100

    Handle unknown port types in the legacy endpoint

--- asusrouter/modules/endpoint/ethernet_ports.py
+++ asusrouter/modules/endpoint/ethernet_ports.py
@@ -2,12 +2,15 @@
 
 from __future__ import annotations
 
+import logging
 from typing import Any
 
 from asusrouter.modules.data import AsusData
 from asusrouter.modules.ports import PORT_SPEED, PortSpeed, PortType
 from asusrouter.tools.converters import safe_int
 from asusrouter.tools.readers import read_json_content
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def read(content: str) -> dict[str, Any]:
@@ -38,9 +41,29 @@ def process(data: dict[str, Any]) -> dict[AsusData, Any]:
         }
 
     for port, value in port_speed.items():
-        port_type = PortType(port[0:3].lower())
+        # Get the port code
+        port_code = port[0:3].lower()
+        # Check whether the port code is in PortType enum
+        if port_code in PortType.__members__:
+            port_type = PortType(port_code)
+        else:
+            # This should be some other kind of port and not LAN or WAN
+            # Based on https://github.com/Vaskivskyi/ha-asusrouter/issues/774
+            # it is probably the SFPP port, since 10G WAN/LAN should be
+            # detected properly.
+            if port_code == "10g":
+                port_type = PortType.SFPP
+            else:
+                continue
+
+            # If the port type is not in the ports dict, add it
+            if port_type not in ports:
+                ports[port_type] = {}
+
+        # Get the port id and link rate
         port_id = safe_int(port[3:])
         link_rate = PORT_SPEED.get(value)
+        # Save the port info
         ports[port_type][port_id] = {
             "state": link_rate not in (PortSpeed.LINK_DOWN, PortSpeed.UNKNOWN),
             "link_rate": link_rate,
