4f58b0f1916e0bc84fc0e78d6574cb79b03e1473
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2024-01-28 19:03:18 +0100

    Add FW release notes when update available

---
+++ asusrouter/modules/endpoint/firmware_note.py
@@ -0,0 +1,46 @@
+"""Firmware release notes endpoint module."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from asusrouter.modules.data import AsusData
+from asusrouter.tools.converters import clean_string
+
+
+def read(content: str) -> dict[str, Any]:
+    """Read firmware release note data."""
+
+    raw_note = content.replace("\ufeff", "")
+    raw_note = raw_note.replace("\uff1a", ":")  # Replace the full-width colon
+
+    # Get only the changes
+    # 1. Split the data into lines
+    lines = raw_note.splitlines()
+    # 2. Clean the lines
+    clean_lines = []
+    for line in lines:
+        if "Firmware version" in line or "Release Note" in line:
+            continue
+        clean_line = clean_string(line)
+        if clean_line:
+            clean_lines.append(clean_line)
+    # 3. Combine the lines into a single string again
+    note = "\n".join(clean_lines)
+
+    release_note: dict[str, Any] = {
+        "release_note": note,
+        "release_note_raw": raw_note,
+    }
+
+    return release_note
+
+
+def process(data: dict[str, Any]) -> dict[AsusData, Any]:
+    """Process firmware release note data."""
+
+    state: dict[AsusData, Any] = {
+        AsusData.FIRMWARE_NOTE: data,
+    }
+
+    return state
