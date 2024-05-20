a6548414a54002d403a47408619b9e5d4b903101
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2024-05-20 19:43:23 +0200

    Add tests

--- tests/modules/test_system.py
+++ tests/modules/test_system.py
@@ -1,12 +1,17 @@
 """Tests for the system module."""
 
-from unittest.mock import AsyncMock
+from unittest import mock
 
 import pytest
 
-from asusrouter.modules.system import STATE_MAP, AsusSystem, set_state
+from asusrouter.modules.system import (
+    STATE_MAP,
+    AsusSystem,
+    AsusSystemDeprecated,
+    set_state,
+)
 
-async_callback = AsyncMock()
+async_callback = mock.AsyncMock()
 
 
 @pytest.mark.asyncio
@@ -30,6 +35,43 @@ async def test_set_state(state, expected_args):
 
     # Test set_state with the given state
     await set_state(async_callback, state)
+    async_callback.assert_called_once_with(**expected_args)
+
+    # Reset the mock callback function
+    async_callback.reset_mock()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "deprecated_state, repl",
+    AsusSystemDeprecated.items(),
+)
+async def test_set_state_deprecated(deprecated_state, repl):
+    """Test set_state with a deprecated state."""
+
+    repl_state, repl_ver = repl
+
+    # Prepare the expected warning message
+    message = f"Deprecated state `{deprecated_state.name}` from `AsusSystem` \
+enum used. Use `{repl_state.name}` instead"
+    if repl_ver:
+        message += f". This state will be removed in version {repl_ver}"
+
+    # Prepare the expected arguments for the callback function
+    expected_args = STATE_MAP.get(
+        repl_state,
+        {
+            "service": repl_state.value,
+            "arguments": {},
+            "apply": True,
+            "expect_modify": False,
+        },
+    )
+
+    # Test set_state with the deprecated state
+    with mock.patch("asusrouter.modules.system._LOGGER.warning") as mock_warning:
+        await set_state(async_callback, deprecated_state)
+    mock_warning.assert_called_once_with(message)
     async_callback.assert_called_once_with(**expected_args)
 
     # Reset the mock callback function
