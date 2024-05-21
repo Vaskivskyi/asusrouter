bb4143439fb95cb89a71e00e8a4f1e550959c85a
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2024-05-21 19:33:04 +0200

    1.10.0 (#501)
    * Bump codecov/codecov-action from 4.3.0 to 4.3.1 (#491)
    
    Bumps [codecov/codecov-action](https://github.com/codecov/codecov-action) from 4.3.0 to 4.3.1.
    - [Release notes](https://github.com/codecov/codecov-action/releases)
    - [Changelog](https://github.com/codecov/codecov-action/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/codecov/codecov-action/compare/v4.3.0...v4.3.1)
    
    ---
    updated-dependencies:
    - dependency-name: codecov/codecov-action
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump actions/checkout from 4.1.5 to 4.1.6 (#496)
    
    Bumps [actions/checkout](https://github.com/actions/checkout) from 4.1.5 to 4.1.6.
    - [Release notes](https://github.com/actions/checkout/releases)
    - [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/actions/checkout/compare/v4.1.5...v4.1.6)
    
    ---
    updated-dependencies:
    - dependency-name: actions/checkout
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump codecov/codecov-action from 4.3.1 to 4.4.0 (#495)
    
    Bumps [codecov/codecov-action](https://github.com/codecov/codecov-action) from 4.3.1 to 4.4.0.
    - [Release notes](https://github.com/codecov/codecov-action/releases)
    - [Changelog](https://github.com/codecov/codecov-action/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/codecov/codecov-action/compare/v4.3.1...v4.4.0)
    
    ---
    updated-dependencies:
    - dependency-name: codecov/codecov-action
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Move the module imports into separate thread (#497)
    
    * Improve exception messages in the connection module (#498)
    
    * Bump codecov/codecov-action from 4.4.0 to 4.4.1 (#500)
    
    Bumps [codecov/codecov-action](https://github.com/codecov/codecov-action) from 4.4.0 to 4.4.1.
    - [Release notes](https://github.com/codecov/codecov-action/releases)
    - [Changelog](https://github.com/codecov/codecov-action/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/codecov/codecov-action/compare/v4.4.0...v4.4.1)
    
    ---
    updated-dependencies:
    - dependency-name: codecov/codecov-action
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Add AiMesh reboot (#499)
    
    * Bump version to `1.10.0`
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- tests/modules/test_system.py
+++ tests/modules/test_system.py
@@ -1,12 +1,17 @@
 """Tests for the system module."""
 
-from unittest.mock import AsyncMock
+from unittest import mock
 
 import pytest
 
-from asusrouter.modules.system import STATE_MAP, AsusSystem, set_state
+from asusrouter.modules.system import (
+    STATE_MAP,
+    AsusSystem,
+    AsusSystemDeprecated,
+    set_state,
+)
 
-async_callback = AsyncMock()
+async_callback = mock.AsyncMock()
 
 
 @pytest.mark.asyncio
@@ -31,6 +36,49 @@ async def test_set_state(state, expected_args):
     # Test set_state with the given state
     await set_state(async_callback, state)
     async_callback.assert_called_once_with(**expected_args)
+
+    # Reset the mock callback function
+    async_callback.reset_mock()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "deprecated_state, repl_state, repl_ver",
+    [
+        (AsusSystem.REBUILD_AIMESH, AsusSystem.AIMESH_REBUILD, None),
+        (AsusSystem.REBUILD_AIMESH, AsusSystem.AIMESH_REBUILD, "1.0.0"),
+    ],
+)
+async def test_set_state_deprecated(deprecated_state, repl_state, repl_ver):
+    """Test set_state with a deprecated state."""
+
+    # Prepare the expected warning message
+    message = f"Deprecated state `{deprecated_state.name}` from `AsusSystem` \
+enum used. Use `{repl_state.name}` instead"
+    if repl_ver is not None:
+        message += f". This state will be removed in version {repl_ver}"
+
+    # Prepare the expected arguments for the callback function
+    expected_args = STATE_MAP.get(
+        repl_state,
+        {
+            "service": repl_state.value,
+            "arguments": {},
+            "apply": True,
+            "expect_modify": False,
+        },
+    )
+
+    # Mock the AsusSystemDeprecated enum
+    with mock.patch.dict(
+        "asusrouter.modules.system.AsusSystemDeprecated",
+        {deprecated_state: (repl_state, repl_ver)},
+    ):
+        # Test set_state with the deprecated state
+        with mock.patch("asusrouter.modules.system._LOGGER.warning") as mock_warning:
+            await set_state(async_callback, deprecated_state)
+        mock_warning.assert_called_once_with(message)
+        async_callback.assert_called_once_with(**expected_args)
 
     # Reset the mock callback function
     async_callback.reset_mock()
