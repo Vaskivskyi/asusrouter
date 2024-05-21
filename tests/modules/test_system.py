20ba41e474a2551a681f850675e89744c1c238c5
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2024-05-21 19:11:20 +0200

    Update tests

--- tests/modules/test_system.py
+++ tests/modules/test_system.py
@@ -43,18 +43,19 @@ async def test_set_state(state, expected_args):
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    "deprecated_state, repl",
-    AsusSystemDeprecated.items(),
+    "deprecated_state, repl_state, repl_ver",
+    [
+        (AsusSystem.REBUILD_AIMESH, AsusSystem.AIMESH_REBUILD, None),
+        (AsusSystem.REBUILD_AIMESH, AsusSystem.AIMESH_REBUILD, "1.0.0"),
+    ],
 )
-async def test_set_state_deprecated(deprecated_state, repl):
+async def test_set_state_deprecated(deprecated_state, repl_state, repl_ver):
     """Test set_state with a deprecated state."""
-
-    repl_state, repl_ver = repl
 
     # Prepare the expected warning message
     message = f"Deprecated state `{deprecated_state.name}` from `AsusSystem` \
 enum used. Use `{repl_state.name}` instead"
-    if repl_ver:
+    if repl_ver is not None:
         message += f". This state will be removed in version {repl_ver}"
 
     # Prepare the expected arguments for the callback function
@@ -68,11 +69,16 @@ enum used. Use `{repl_state.name}` instead"
         },
     )
 
-    # Test set_state with the deprecated state
-    with mock.patch("asusrouter.modules.system._LOGGER.warning") as mock_warning:
-        await set_state(async_callback, deprecated_state)
-    mock_warning.assert_called_once_with(message)
-    async_callback.assert_called_once_with(**expected_args)
+    # Mock the AsusSystemDeprecated enum
+    with mock.patch.dict(
+        "asusrouter.modules.system.AsusSystemDeprecated",
+        {deprecated_state: (repl_state, repl_ver)},
+    ):
+        # Test set_state with the deprecated state
+        with mock.patch("asusrouter.modules.system._LOGGER.warning") as mock_warning:
+            await set_state(async_callback, deprecated_state)
+        mock_warning.assert_called_once_with(message)
+        async_callback.assert_called_once_with(**expected_args)
 
     # Reset the mock callback function
     async_callback.reset_mock()
