4eba40cccdabb72a5dc21f4d7007e1d1cb6488f2
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2024-08-24 12:30:51 +0200

    Remove code duplication in generic endpoint `read` (#521)

--- asusrouter/modules/endpoint/aura.py
+++ asusrouter/modules/endpoint/aura.py
@@ -2,15 +2,4 @@
 
 from __future__ import annotations
 
-from typing import Any
-
-from asusrouter.tools.readers import read_json_content
-
-
-def read(content: str) -> dict[str, Any]:
-    """Read aura data"""
-
-    # Read the json content
-    aura: dict[str, Any] = read_json_content(content)
-
-    return aura
+from asusrouter.tools.readers import read_json_content as read  # noqa: F401
--- tests/modules/endpoint/test_hook.py
+++ tests/modules/endpoint/test_hook.py
@@ -1,6 +1,6 @@
-"""Tests for the Aura endpoint module."""
+"""Tests for the Hook endpoint module."""
 
-from asusrouter.modules.endpoint.aura import read
+from asusrouter.modules.endpoint.hook import read
 
 from ._test_default import _test_read
 
