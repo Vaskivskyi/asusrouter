94a737bf6ea37103b10e2d2089a21eab9e08dd8d
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2026-06-13 18:53:24 +0200

    Add IPAddress identifier

--- asusrouter/tools/identifiers/__init__.py
+++ asusrouter/tools/identifiers/__init__.py
@@ -2,8 +2,10 @@
 
 from __future__ import annotations
 
+from asusrouter.tools.identifiers.ip import IpAddress
 from asusrouter.tools.identifiers.mac import MacAddress
 
 __all__ = [
+    "IpAddress",
     "MacAddress",
 ]
