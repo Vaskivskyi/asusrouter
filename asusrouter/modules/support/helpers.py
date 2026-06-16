7b60b86e3d07fcce0b2ec89ae180221c24adfbee
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2026-06-16 20:35:49 +0200

    Add helpers for checking support data

--- asusrouter/modules/support/helpers.py
+++ asusrouter/modules/support/helpers.py
@@ -1,10 +1,11 @@
-"""Helper factories for support translators."""
+"""Helper for the support module."""
 
 from __future__ import annotations
 
 from collections.abc import Callable
 from typing import Any, TypeVar
 
+from asusrouter.modules.support.flag import ARSupportType
 from asusrouter.tools.converters import safe_int_nn
 from asusrouter.tools.readers import is_true_in_dict
 
@@ -65,3 +66,28 @@ def make_list_translator(
         ]
 
     return translate
+
+
+def support_available(
+    support: dict[ARSupportType, Any], key: ARSupportType
+) -> bool:
+    """Return True if the bool support flag is set."""
+
+    return support.get(key) is True
+
+
+def support_available_in(
+    support: dict[ARSupportType, Any], key: ARSupportType, item: Any
+) -> bool:
+    """Return True if item is present in the list under the given key."""
+
+    value = support.get(key)
+    return isinstance(value, list) and item in value
+
+
+def support_value(
+    support: dict[ARSupportType, Any], key: ARSupportType
+) -> Any:
+    """Return the support value for the given key."""
+
+    return support.get(key)
