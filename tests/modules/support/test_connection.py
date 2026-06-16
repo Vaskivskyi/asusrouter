a62493570a94659b01543d2baa4c758d052fb356
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2026-06-16 17:27:53 +0200

    Optimise translate_connection via factory

--- tests/modules/support/test_connection.py
+++ tests/modules/support/test_connection.py
@@ -14,39 +14,9 @@ from asusrouter.modules.support.flag import ARSupportValue
 @pytest.mark.parametrize(
     ("data", "expected"),
     [
+        ({ARSupportValue.CONNECTION_HTTPS.value: 1}, [ARConnection.HTTPS]),
+        ({ARSupportValue.CONNECTION_SSH.value: 1}, [ARConnection.SSH]),
         ({}, []),
-        ({ARSupportValue.CONNECTION_HTTPS: 1}, [ARConnection.HTTPS]),
-        ({ARSupportValue.CONNECTION_SSH: 1}, [ARConnection.SSH]),
-        (
-            {
-                ARSupportValue.CONNECTION_HTTPS: 1,
-                ARSupportValue.CONNECTION_SSH: 1,
-            },
-            [ARConnection.HTTPS, ARConnection.SSH],
-        ),
-        (
-            {
-                ARSupportValue.CONNECTION_HTTPS: 0,
-                ARSupportValue.CONNECTION_SSH: 0,
-            },
-            [],
-        ),
-        (
-            {
-                ARSupportValue.CONNECTION_HTTPS: True,
-                ARSupportValue.CONNECTION_SSH: False,
-            },
-            [ARConnection.HTTPS],
-        ),
-        (
-            {
-                ARSupportValue.CONNECTION_HTTPS: "0",
-                ARSupportValue.CONNECTION_SSH: "1",
-            },
-            [ARConnection.SSH],
-        ),
-        # Not a dict
-        ("not_a_dict", []),
     ],
 )
 def test_translate_connection(
@@ -54,5 +24,4 @@ def test_translate_connection(
 ) -> None:
     """Test translate_connection returns correct connection types."""
 
-    result = translate_connection(data)
-    assert set(result) == set(expected)
+    assert translate_connection(data) == expected
